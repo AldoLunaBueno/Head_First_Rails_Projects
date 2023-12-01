ca7ac4f9f5dd23394feee99ff3228a913f1b95bc
AldoLunaBueno <alunab@uni.pe>
2023-11-30 22:55:44 -0500

    Done Chapter 3: Inserting, updating and deleting (mebay)

---
+++ mebay/config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get '/ads/new', to: 'ads#new'
+  post '/ads/create', to: 'ads#create'
+  get '/ads/:id/edit', to: 'ads#edit'
+  put '/ads/:id/update', to: 'ads#update'
+  get '/ads/:id/delete', to: 'ads#destroy' # delete method
+  get '/ads/:id', to: 'ads#show'
+  get '/ads/', to: 'ads#index'
+
+end
